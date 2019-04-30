487717131ee69bd8a618af32cc4a7d9dffa147fa
Nigol <49141808+raitnigol@users.noreply.github.com>
2019-04-30 17:04:38 +0300

    Add files via upload
    added a shorter version of the code.

---
+++ fo76_nukecodes_shorter.py
@@ -0,0 +1,14 @@
+#import requests and json to pull info from nukacrpyt json file 
+
+import requests; import json
+
+# get the request from nukacrypt and load the content
+
+r = requests.get('https://nukacrypt.com/codes.json')
+j = json.loads(r.content)
+
+# print the outcome 
+
+print("Site Alpha nuke code: " + j['alpha'])
+print("Site Bravo nuke code: " + j['bravo'])
+print("Site Charlie nuke code: " + j['charlie'])
