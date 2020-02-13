6e5e8db62828cc087c46f87074a520e319a50610
Nigol <rait.nigol@khk.ee>
2020-02-13 15:45:20 +0200

    Update fo76_nukecodes.py
    remove trailing whiteline

--- fo76_nukecodes.py
+++ fo76_nukecodes.py
@@ -4,7 +4,7 @@ import json
 # get the request from nukacrypt and load the content
 j = json.loads(requests.get('https://nukacrypt.com/codes.json').content)
 
-# print the outcome 
+# print the outcome
 print("Site Alpha nuke code: " + j['alpha'])
 print("Site Bravo nuke code: " + j['bravo'])
 print("Site Charlie nuke code: " + j['charlie'])
